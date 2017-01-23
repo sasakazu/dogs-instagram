f5e58c93b6517f86ce1a322206d4e7c23d4a20f0
sasakazu <dropout.college2014@gmail.com>
2017-01-23 13:29:53 +0900

    remove remember_digest done.....

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170119092252) do
+ActiveRecord::Schema.define(version: 20170123042713) do
 
   create_table "dogposts", force: :cascade do |t|
     t.datetime "created_at", null: false
